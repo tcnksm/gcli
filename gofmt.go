1f3c93260129dd55b8ec194e11e2086abc93fd96
tcnksm <nsd22843@gmail.com>
2014-07-13 20:50:15 +0900

    Replate gofmt command with go/printer and go/parser

--- gofmt.go
+++ gofmt.go
@@ -1,18 +1,88 @@
 package main
 
 import (
+	"bytes"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"io"
 	"io/ioutil"
-	"os/exec"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-func GoFmt(dir string) error {
-	cmd := exec.Command("gofmt", "-w", dir)
-	cmd.Stdout = ioutil.Discard
-	cmd.Stderr = ioutil.Discard
+func processFile(filename string, in io.Reader, out io.Writer, stdin bool) error {
+	if in == nil {
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		in = f
+	}
 
-	if err := cmd.Run(); err != nil {
+	src, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+	fileSet := token.NewFileSet()
+	file, err := parser.ParseFile(fileSet, filename, src, parser.ParseComments)
+	if err != nil {
 		return err
 	}
 
+	ast.SortImports(fileSet, file)
+
+	var buf bytes.Buffer
+	tabWidth := 8
+	printerMode := printer.UseSpaces | printer.TabIndent
+	err = (&printer.Config{Mode: printerMode, Tabwidth: tabWidth}).Fprint(&buf, fileSet, file)
+	if err != nil {
+		return err
+	}
+
+	res := buf.Bytes()
+	if !bytes.Equal(src, res) {
+		err = ioutil.WriteFile(filename, res, 0)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func isGofile(f os.FileInfo) bool {
+	name := f.Name()
+	return !f.IsDir() && !strings.HasPrefix(name, ".") && strings.HasSuffix(name, ".go")
+}
+
+func visitFile(path string, f os.FileInfo, err error) error {
+
+	if err == nil && isGofile(f) {
+		err = processFile(path, nil, os.Stdout, false)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func walkDir(path string) {
+	filepath.Walk(path, visitFile)
+}
+
+func GoFmt(path string) error {
+	dir, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if dir.IsDir() {
+		walkDir(path)
+	}
 	return nil
 }
