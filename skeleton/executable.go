30e2fec09c894480e9df49846d71389f9ba1a03d
tcnksm <nsd22843@gmail.com>
2015-08-25 19:12:45 +0900

    Add more field for design template

--- skeleton/executable.go
+++ skeleton/executable.go
@@ -1,5 +1,18 @@
 package skeleton
 
+import (
+	"fmt"
+	"reflect"
+)
+
+const (
+	// DefaultVersion is default appliaction version
+	DefaultVersion = "0.1.0"
+
+	// defaultVersion is default application description
+	DefaultDescription = ""
+)
+
 // Executable store executable meta information
 type Executable struct {
 	// Name is executable name
@@ -19,4 +32,29 @@ type Executable struct {
 
 	// Description is description of the executable
 	Description string
+
+	// FrameworkStr is framework name to use
+	FrameworkStr string `toml:"Framework"`
+}
+
+func NewExecutable() *Executable {
+	return &Executable{
+		Version:     DefaultVersion,
+		Description: DefaultDescription,
+	}
+}
+
+// Overwrite overwrites provided value with default value
+func (e *Executable) Overwrite(key string, v interface{}) error {
+	// Check
+	switch v.(type) {
+	case string, []Command, []Flag:
+	default:
+		return fmt.Errorf("unexpected value: %#v", v)
+	}
+
+	rve := reflect.ValueOf(e)
+	rve.Elem().FieldByName(key).Set(reflect.ValueOf(v))
+
+	return nil
 }
